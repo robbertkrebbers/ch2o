a6a0b82ffa4b76f85436871a7318e7a348a8efd1
Robbert Krebbers <mail@robbertkrebbers.nl>
2014-08-09 23:44:04 +0200

    Extract strings.

--- extraction.v
+++ extraction.v
@@ -1,6 +1,6 @@
 (* Copyright (c) 2012-2014, Robbert Krebbers. *)
 (* This file is distributed under the terms of the BSD license. *)
-Require Import interpreter ExtrOcamlBasic.
+Require Import interpreter ExtrOcamlBasic ExtrOcamlString.
 
 Cd "extraction".
 Extraction Blacklist list.
