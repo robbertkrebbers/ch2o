7267acf6d0f0fbb8e67edec3a77f4acbf866a2d4
Robbert Krebbers <mail@robbertkrebbers.nl>
2014-08-24 22:37:33 +0200

    Move multistep function from executable.v to interpreter.v.

--- executable.v
+++ executable.v
@@ -1,10 +1,9 @@
 (* Copyright (c) 2012-2014, Robbert Krebbers. *)
 (* This file is distributed under the terms of the BSD license. *)
-Require Export smallstep streams.
-Require Import hashset.
+Require Export smallstep.
 
 Section exec.
-Context `{EnvSpec Ti} (hash : state Ti → Z).
+Context `{EnvSpec Ti}.
 
 Definition assign_exec (Γ : env Ti) (m : mem Ti) (a : addr Ti)
     (v : val Ti) (ass : assign) : option (val Ti * val Ti) :=
@@ -194,17 +193,6 @@ Definition cstep_exec (Γ : env Ti) (δ : funenv Ti)
     end
   | Undef _ => ∅
   end.
-Definition cstep_exec' (Γ : env Ti) (δ : funenv Ti)
-    (S : state Ti) : listset (state Ti) * listset (state Ti) :=
-  let Ss := cstep_exec Γ δ S in
-  if decide (listset_car Ss = []) then ({[ S ]}, ∅) else (∅, Ss).
-Definition csteps_exec (Γ : env Ti) (δ : funenv Ti) :
-    listset (state Ti) → stream (listset (state Ti) * listset (state Ti)) :=
-  cofix go Ss :=
-  let Sss := cstep_exec' Γ δ <$> Ss in
-  let nfs := listset_normalize hash (Sss ≫= fst) in
-  let reds := listset_normalize hash (Sss ≫= snd) in
-  (nfs,reds) :.: go reds.
 
 Lemma assign_exec_correct Γ m a v ass v' va' :
   assign_exec Γ m a v ass = Some (v',va') ↔ assign_sem Γ m a v ass v' va'.
--- interpreter.v
+++ interpreter.v
@@ -1,6 +1,6 @@
 (* Copyright (c) 2012-2014, Robbert Krebbers. *)
 (* This file is distributed under the terms of the BSD license. *)
-Require Import String.
+Require Import String hashset streams.
 Require Export executable frontend natural_type_environment two_complement.
 Local Open Scope string_scope.
 
@@ -13,6 +13,17 @@ Context  (hash : state Ti → Z).
 Let interpreter_env := (natural_env ptr_size align_base).
 Existing Instance interpreter_env.
 
+Definition cstep_exec' (Γ : env Ti) (δ : funenv Ti)
+    (S : state Ti) : listset (state Ti) * listset (state Ti) :=
+  let Ss := cstep_exec Γ δ S in
+  if decide (listset_car Ss = []) then ({[ S ]}, ∅) else (∅, Ss).
+Definition csteps_exec (Γ : env Ti) (δ : funenv Ti) :
+    listset (state Ti) → stream (listset (state Ti) * listset (state Ti)) :=
+  cofix go Ss :=
+  let Sss := cstep_exec' Γ δ <$> Ss in
+  let nfs := listset_normalize hash (Sss ≫= fst) in
+  let reds := listset_normalize hash (Sss ≫= snd) in
+  (nfs,reds) :.: go reds.
 Definition interpreter (Θ : list (N * decl Ti))
     (f : funname) (vs : list (val Ti)) :
     string + stream (listset (state Ti) * listset (state Ti)) :=
@@ -23,5 +34,5 @@ Definition interpreter (Θ : list (N * decl Ti))
     "interpreter called with values that cannot be typed";
   guard (σs' = (σs : list (type Ti)))
     with "interpreter called with values of incorrect type";
-  inr (csteps_exec hash Γ δ {[ initial_state m f vs ]}).
+  inr (csteps_exec Γ δ {[ initial_state m f vs ]}).
 End interpreter.
