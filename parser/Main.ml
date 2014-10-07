robbertkrebbers/ch2o

commit 1e9aa628411248ef0905873e1e77e8d2ad9f72cc

Do not translate Cabs.NOTHING into dummy.

parser/Main.ml
@@ -678,7 +678,6 @@ and cexpr_of_expression x =
   | Cabs.MEMBEROF (y,f) -> CEField (cexpr_of_expression y,chars_of_string f)
   | Cabs.MEMBEROFPTR (y,f) ->
       CEField (CEDeref (cexpr_of_expression y),chars_of_string f)
-  | Cabs.NOTHING -> econst1
   | _ -> raise (Unknown_expression x)
 
 and ref_of_initwhat iw =

commit 5496d8e32a0a660ad64050d4ab8dde4b8dbdfda2

Fix bug in translation of for caused by 1e9aa628.

parser/Main.ml
@@ -739,7 +739,8 @@ let rec cstmt_of_statements l =
         (cstmt_of_statements l')
   | Cabs.FOR (Cabs.FC_DECL (Cabs.DECDEF ((t,l),_)),e2,e3,y,z)::l' ->
       cscomp (CSScope (fold_defs [] t l
-          [Cabs.FOR (Cabs.FC_EXP Cabs.NOTHING,e2,e3,y,z)]))
+          [Cabs.FOR (Cabs.FC_EXP (Cabs.CONSTANT (Cabs.CONST_INT "0")),
+           e2,e3,y,z)]))
         (cstmt_of_statements l')
   | Cabs.DOWHILE (e,y,_)::l' ->
       cscomp (CSDoWhile (cstmt_of_statements [y],