48a9b75e82c5ecf2dc2b04f760ec7e5c14cba326
Robbert Krebbers <mail@robbertkrebbers.nl>
2015-09-18 11:10:59 +0200

    State adequacy in a slightly nicer way.

--- axiomatic/axiomatic_adequate.v
+++ axiomatic/axiomatic_adequate.v
@@ -7,13 +7,12 @@ Local Hint Extern 1 (⊥ _) => solve_mem_disjoint.
 Local Hint Extern 1 (sep_valid _) => solve_mem_disjoint.
 Local Hint Extern 1 (_ ≤ _) => omega.
 
-(** * Partial program correctness *)
-(** We prove that the interpretation of the statement Hoare judgment indeed
-implies partial program correctness. *)
-Lemma ax_stmt_sound `{EnvSpec K} Γ δ P Q s m S' cmτ :
-  ✓ Γ → ✓{Γ,'{m} } δ → ✓{Γ} m → mem_locks m = ∅ → (Γ,'{m},[]) ⊢ s : cmτ →
-  Γ\ δ ⊨ₛ {{ P }} s {{ Q }} →
-  (∀ n, assert_holds P Γ '{m} δ [] n (cmap_erase m)) →
+(** * Adequacy *)
+(** We prove that the Hoare judgment indeed implies partial program
+correctness. *)
+Lemma ax_stmt_adequate `{EnvSpec K} Γ δ Q s m S' cmτ :
+  ✓ Γ → ✓{Γ,'{m}} δ → ✓{Γ} m → mem_locks m = ∅ → (Γ,'{m},[]) ⊢ s : cmτ →
+  Γ\ δ ⊨ₛ {{ assert_eq_mem (cmap_erase m) }} s {{ Q }} →
   Γ\ δ ⊢ₛ State [] (Stmt ↘ s) m ⇒* S' →
   (**i 1.) *) (∃ n' m',
     S' = State [] (Stmt ↗ s) m' ∧
@@ -23,7 +22,7 @@ Lemma ax_stmt_sound `{EnvSpec K} Γ δ P Q s m S' cmτ :
     assert_holds (Q v) Γ '{m'} δ [] n' (cmap_erase m')) ∨
   (**i 3.) *) red (cstep Γ δ) S'.
 Proof.
-  intros ?. revert m S'. cut (∀ n1 n2 k φ m S' τf,
+  intros ?. revert m S'. cut (∀ P n1 n2 k φ m S' τf,
     ✓{Γ,'{m} } δ →
     (Γ,'{m},locals k.*2) ⊢ φ : τf → (Γ,'{m}) ⊢ k : τf ↣ Stmt_type cmτ →
     ✓{Γ} m → maybe Undef φ = None → focus_locks_valid φ m →
@@ -34,19 +33,20 @@ Proof.
       maybe Undef φ' = None ∧ focus_locks_valid φ' m' ∧ ✓{Γ} m' ∧
       ax_graph ax_disjoint_cond (ax_stmt_post (dassert_pack_top P Q) s cmτ)
         Γ δ '{m'} [] n2 k' φ' m').
-  { intros help m S' ?????? p.
+  { intros help m S' ???? Hax p.
     apply csteps_rcsteps, rtc_bsteps in p; destruct p as [n p].
-    destruct (help n 1 [] (Stmt ↘ s) m S' (Stmt_type cmτ))
-      as (k'&φ'&m'&->&?&?&?&Hax'); done || eauto.
+    destruct (help (assert_eq_mem (cmap_erase m)) n 1 [] (Stmt ↘ s) m S'
+      (Stmt_type cmτ)) as (k'&φ'&m'&->&?&?&?&Hax'); done || eauto.
     { by repeat typed_constructor. }
     { typed_constructor. }
+    { by apply Hax. }
     inversion Hax' as [|??? [d' ????]|???? Hred]; clear Hax'; subst.
     { destruct d'; naive_solver. }
     do 2 right; apply (red_subrel (rcstep Γ δ [])); auto using rcstep_cstep.
     apply (Hred '{m'} 0 _ ∅); auto.
     split; rewrite ?sep_right_id by auto;
       eauto using cmap_valid_memenv_valid, cmap_empty_valid. }
-  intros n1 n2; induction n1 as [n1 IH] using lt_wf_ind.
+  intros P n1 n2; induction n1 as [n1 IH] using lt_wf_ind.
   intros k φ m S' τf ?????? Hax p.
   inv_rcsteps p as [|n' ? S ? p1 p2]; eauto 10 using mk_ax_next, ax_weaken.
   assert (ax_next ax_disjoint_cond (ax_stmt_post (dassert_pack_top P Q) s cmτ)
